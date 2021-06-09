df9ac656f195a8d8ec65946f73b72eb04833a6c2
El <mattianc@hotmail.it>
2021-06-09 17:39:12 +0200

    tutto

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ $(window).scroll(function (e) {
     console.log(currentScrollPosition);
 });
 
+// Check native Lazy-loading
+if ('loading' in HTMLImageElement.prototype) {
+    // Supported
+
+    const images = document.querySelectorAll('img[loading="lazy"]')
+    images.forEach(img => {
+        img.src = img.dataset.src;
+    })
+} else {
+    // Not supported
+
+    const script = document.createElement('script')
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js';
+    document.body.appendChild(script);
+}
+
 class HoverButton {
     constructor(el) {
     this.el = el;
@@ -92,7 +108,7 @@ const btn6 = document.querySelector('.custom-element:nth-child(6)');
 new HoverButton(btn6);
 
 
-// VideoMove on mouse hover
+// Videomove on mouse hover
 class HoverButtonVideo {
 constructor(el) {
     this.el = el;
@@ -162,12 +178,90 @@ onLeave() {
     this.el.style.zIndex = 2;
 }
 }
-
 const btnvideo = document.querySelector('.video');
 new HoverButtonVideo(btnvideo);
-
-
 
 $('#player').click(function () {
     $('.video').toggleClass('show')
 });
+
+//Toggle button
+$('.interessi').click(toggleviewInt);
+$('.colleghi').click(toggleviewCol);
+
+function toggleviewInt(){
+    var toggleInteressi = $('.interessi');
+    if(!toggleInteressi.hasClass("selected")){
+     toggleInteressi.addClass('selected');
+     $('.colleghi').removeClass('selected');
+    }   
+}
+function toggleviewCol(){
+    var toggleColleghi = $('.colleghi');
+    if(!toggleColleghi.hasClass('selected')){
+        toggleColleghi.addClass('selected');
+        $('.interessi').removeClass('selected');
+    }   
+}
+
+
+//Iphone animation
+// Add scrollmagic controller
+let controller = new ScrollMagic.Controller();
+
+// Add timeline
+let tl2 = anime.timeline({autoplay: false});
+
+// Add animations
+let s2a1 = {
+  targets: '#two .elem img',
+  scale: [0.7,0.25],
+  duration: 700,
+  delay: 0,
+  easing: 'easeInOutSine',
+  translateY: {
+    value: ['40%', '0%'],}
+
+
+};
+
+let s2a2 = {
+  targets: '#two .elem img',
+  scale: 0.25,
+  duration: 500,
+};
+
+// Add children
+tl2.add(s2a1).add(s2a2);
+
+// Get section height
+let twoHeight = document.getElementById("two").clientHeight;
+console.log('twoHeight: ' + twoHeight);
+
+//------------------
+//SCENE 2
+//------------------
+
+//Add second scrollmagic scene
+let scene2 = new ScrollMagic.Scene({
+  triggerElement: "#two",
+  duration: 4500,
+  triggerHook: 0,
+})
+
+// Add debug indicators
+.addIndicators({
+  colorTrigger: "black",
+  colorStart: "blue",
+  colorEnd: "red",
+  indent: 10
+})
+
+// Trigger animation timeline
+//Use scroll position to play animation
+.on("progress", function (event) {
+  tl2.seek(tl2.duration * event.progress);
+})
+
+.setPin('#two')
+.addTo(controller);

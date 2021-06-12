6647c1beda6088d50f9d4de8e975bcbb4ffc2490
El <mattianc@hotmail.it>
2021-06-12 20:20:00 +0200

    aaa

--- js/script.js
+++ js/script.js
@@ -266,14 +266,10 @@ let s4a1 = {
 }
 
 // Add children
-// tl2.add(s2a1);
 tl2.add(s2a1);
 tl3.add(s3a1);
 tl4.add(s4a1);
 
-// Get section height
-// let twoHeight = document.getElementById("two").clientHeight;
-// console.log('twoHeight: ' + twoHeight);
 
 //Add scrollmagic scene
 let scene2 = new ScrollMagic.Scene({
@@ -300,6 +296,86 @@ let scene2 = new ScrollMagic.Scene({
 .setPin('#smWrapper')
 .addTo(controller);
 
+
+
+
+
+
+
+
+//Iphone animation
+// Add scrollmagic controller
+let controller_c = new ScrollMagic.Controller();
+
+// Add timeline
+let tl2_c = anime.timeline({autoplay: false});
+let tl3_c = anime.timeline({autoplay: false});
+let tl4_c = anime.timeline({autoplay: false});
+
+// Add animations
+let s2a1_c = {
+  targets: '#iphone-mockup1',
+  scale: [1.1, 0.5],
+  duration: 700,
+  delay: 0,
+  easing: 'easeInOutSine',
+  translateY: {
+    value: ['30%', '-10%'],}
+};
+
+let s3a1_c = {
+    targets: '.text-left_c',
+    translateX: {
+        value: ['0%', '70%'],
+    },
+    easing: 'linear',
+    delay: 700,
+    duration: 200,
+}
+let s4a1_c = {
+    targets: '.text-right_c',
+    translateX: {
+        value: ['0%', '-70%'],
+    },
+    easing: 'linear',
+    delay: 500,
+    duration: 200,
+}
+
+// Add children
+tl2_c.add(s2a1_c);
+tl3_c.add(s3a1_c);
+tl4_c.add(s4a1_c);
+
+
+//Add scrollmagic scene
+let scene2_c = new ScrollMagic.Scene({
+  triggerElement: "#smWrapper_c",
+  duration: 1500,
+  triggerHook: 0.1,
+})
+
+// Add debug indicators
+// .addIndicators({
+//   colorTrigger: "black",
+//   colorStart: "blue",
+//   colorEnd: "red",
+//   indent: 10
+// })
+
+// Trigger animation timeline
+//Use scroll position to play animation
+.on("progress", function (event) {
+  tl2_c.seek(tl2_c.duration * event.progress);
+  tl3_c.seek(tl3_c.duration * event.progress);
+  tl4_c.seek(tl4_c.duration * event.progress);
+})
+.setPin('#smWrapper_c')
+.addTo(controller_c);
+
+
+
+// CAROUSEL
 $(document).ready(function(){
     $('.category-carousel').slick({
         dots: true,

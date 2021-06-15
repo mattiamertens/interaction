0b437e90e0ecd069785bf4c2d83e159e73051707
El <mattianc@hotmail.it>
2021-06-15 12:36:50 +0200

    caratteristiche

--- js/script.js
+++ js/script.js
@@ -26,6 +26,77 @@ $('.sections a, #discover').click(function(){
     }, 500);
 });
 
+// UNDERLINE CURRENT NAVBAR SECTION
+// $(document).ready(function(){
+//     // $sections incleudes all of the container divs that relate to menu items.
+//     var sections = $('.u_section');
+    
+//     // The user scrolls
+//     $(window).scroll(function(){
+      
+//       // currentScroll is the number of pixels the window has been scrolled
+//       var currentScroll = $(this).scrollTop();
+      
+//       // $currentSection is somewhere to place the section we must be looking at
+//       var currentSection
+      
+//       // We check the position of each of the divs compared to the windows scroll positon
+//       sections.each(function(){
+//         // divPosition is the position down the page in px of the current section we are testing      
+//         var divPosition = $(this).offset().top;
+        
+//         // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+//         // the -1 is so that it includes the div 1px before the div leave the top of the window.
+//         if( divPosition - 1 < currentScroll ){
+//           // We have either read the section or are currently reading the section so we'll call it our current section
+//           currentSection = $(this);
+          
+//           // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+//         }
+        
+//         // This is the bit of code that uses the currentSection as its source of ID
+//         var id = currentSection.attr('id');
+//           $('a').removeClass('active');
+//           $("[href=#"+id+"]").addClass('active');
+//         })
+//     });
+// });
+
+// $(window).scroll(function () { //on scroll event on window
+//     var position = $(this).scrollTop(); //position scrolled sofar
+
+//     $('.u_section').each(function() { //for each loop(chcecks for every element with that class)
+//     //for your every section on site or your class of main sections you are targetting
+//         var target = $(this).offset().top;//each element position from top
+//         var targetBot = target + $(this).height();
+
+//         var id = $(this).attr('id'); //this element ID - should be same as data-id on your nav link
+//         $('nav a[id=' + id + ']').removeClass('hovered'); //clearing
+//         if (position >= target && targetBot >= position) { //if you are scrolled over element with some id 
+//             $('nav a[data-id=' + id + ']').addClass('hovered');
+//             //this will check wich element you are scrolled on and selects in menu that item with same data-id :) 
+//         }
+//     });
+
+// });
+
+// $(function(){
+//     // this will get the full URL at the address bar
+//     var url = window.location.href; 
+//     console.log(scrollTop = $($.attr(this, 'href')))
+
+//     // passes on every "a" tag 
+//     let navlink = $('.navbar .nav-link')
+//     $(navlink).each(function() {
+//             // checks if its the same on the address bar
+//         if(url == (this.href)) { 
+//             $(this).closest(navlink).addClass("active");
+//         }
+//     });
+// });
+
+
+
 class HoverButton {
     constructor(el) {
     this.el = el;

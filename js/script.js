c9a197da136e88c51ee9598372615bb41f243985
El <mattianc@hotmail.it>
2021-06-18 04:26:25 +0200

    font

--- js/script.js
+++ js/script.js
@@ -13,17 +13,12 @@ if ('loading' in HTMLImageElement.prototype) {
     document.body.appendChild(script);
 }
 
-
 // SCROLL TO SECTION
 $('.sections a, #discover').click(function(){
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
-
-
-
-
 
 // UNDERLINE CURRENT NAVBAR SECTION
 const sections = document.querySelectorAll(".u_section");
@@ -146,73 +141,73 @@ new HoverButton(btn7);
 
 // Videomove on mouse hover
 class HoverButtonVideo {
-constructor(el) {
-    this.el = el;
-    this.hover = false;
-    this.calculatePosition();
-    this.attachEventsListener();
-}
-
-attachEventsListener() {
-    window.addEventListener('mousemove', e => this.onMouseMove(e));
-    window.addEventListener('resize', e => this.calculatePosition(e));
-}
-
-calculatePosition() {
-    gsap.set(this.el, {
-    x: 0,
-    y: 0,
-    scale: 1
-    });
-
-    const box = this.el.getBoundingClientRect();
-    this.x = box.left + box.width * 0.5;
-    this.y = box.top + box.height * 0.5;
-    this.width = box.width;
-    this.height = box.height;
-}
-
-onMouseMove(e) {
-    let hover = false;
-    let hoverArea = this.hover ? 1 : 0.5;
-    let x = e.clientX - this.x;
-    let y = e.clientY - this.y;
-    let distance = Math.sqrt(x * x + y * y);
-    if (distance < this.width * hoverArea / 1.6) {
-    hover = true;
-    if (!this.hover) {
-        this.hover = true;
-    }
-    this.onHover(e.clientX, e.clientY);
+    constructor(el) {
+        this.el = el;
+        this.hover = false;
+        this.calculatePosition();
+        this.attachEventsListener();
     }
 
-    if (!hover && this.hover) {
-    this.onLeave();
-    this.hover = false;
+    attachEventsListener() {
+        window.addEventListener('mousemove', e => this.onMouseMove(e));
+        window.addEventListener('resize', e => this.calculatePosition(e));
     }
-}
 
-onHover(x, y) {
-    gsap.to(this.el, {
-    x: (x - this.x) * 0.1,
-    y: (y - this.y) * 0.1,
-    ease: 'power2.out',
-    duration: 0.4
-    });
+    calculatePosition() {
+        gsap.set(this.el, {
+        x: 0,
+        y: 0,
+        scale: 1
+        });
 
-    this.el.style.zIndex = 1001;
-}
-onLeave() {
-    gsap.to(this.el, {
-    x: 0,
-    y: 0,
-    scale: 1,
-    ease: 'elastic.out(1.2, 0.4)',
-    duration: 0.7
-    });
+        const box = this.el.getBoundingClientRect();
+        this.x = box.left + box.width * 0.5;
+        this.y = box.top + box.height * 0.5;
+        this.width = box.width;
+        this.height = box.height;
+    }
 
-    this.el.style.zIndex = 1001;
-}
+    onMouseMove(e) {
+        let hover = false;
+        let hoverArea = this.hover ? 1 : 0.5;
+        let x = e.clientX - this.x;
+        let y = e.clientY - this.y;
+        let distance = Math.sqrt(x * x + y * y);
+        if (distance < this.width * hoverArea / 1.6) {
+        hover = true;
+        if (!this.hover) {
+            this.hover = true;
+        }
+        this.onHover(e.clientX, e.clientY);
+        }
+
+        if (!hover && this.hover) {
+        this.onLeave();
+        this.hover = false;
+        }
+    }
+
+    onHover(x, y) {
+        gsap.to(this.el, {
+        x: (x - this.x) * 0.1,
+        y: (y - this.y) * 0.1,
+        ease: 'power2.out',
+        duration: 0.4
+        });
+
+        this.el.style.zIndex = 1001;
+    }
+    onLeave() {
+        gsap.to(this.el, {
+        x: 0,
+        y: 0,
+        scale: 1,
+        ease: 'elastic.out(1.2, 0.4)',
+        duration: 0.7
+        });
+
+        this.el.style.zIndex = 1001;
+    }
 }
 const btnvideo = document.querySelector('.video');
 new HoverButtonVideo(btnvideo);
@@ -259,15 +254,13 @@ function toggleViewCol(){
     }
     $('.interessi-card').addClass('not-displayed');
     $('.colleghi-card').removeClass('not-displayed');
-    // $('.colleghi-card').show();
-    // $('.interessi-card').hide();
 }
 
 
 //Iphone animation
 // Add scrollmagic controller
 
-window.addEventListener('load', function(){
+window.addEventListener('resize', function(){
     if(window.innerWidth > 768){
 
     let controller = new ScrollMagic.Controller();
@@ -383,14 +376,6 @@ window.addEventListener('load', function(){
         triggerHook: 0.1,
     })
 
-    // Add debug indicators
-    // .addIndicators({
-    //   colorTrigger: "black",
-    //   colorStart: "blue",
-    //   colorEnd: "red",
-    //   indent: 10
-    // })
-
     // Trigger animation timeline
     //Use scroll position to play animation
     .on("progress", function (event) {
@@ -402,23 +387,20 @@ window.addEventListener('load', function(){
     .addTo(controller_c);
     }
 
-    // CAROUSEL
-    $(document).ready(function(){
-        $('.category-carousel').slick({
-            dots: true,
-            infinite: true,
-            arrows: true,
-            slidesToShow: 3,
-            slidesToScroll: 3
-        });
-    });
-
 });
 
+// ICON CAROUSEL
+$(document).ready(function(){
+    $('.category-carousel').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
 
-
-
-
+// JS FOR MOBILE
 window.addEventListener('load', function(){
     if(window.innerWidth < 768){
         $(document).ready(function(){
